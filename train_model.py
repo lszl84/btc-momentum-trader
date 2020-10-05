b730e3565db442e78a9498319fc26da08dba5393
Lukasz <lukasz.sromek@gmail.com>
2020-10-05 19:46:53 +0200

    basic training and predictions

--- train_model.py
+++ train_model.py
@@ -49,17 +49,59 @@ def prepare_dataset(trades_numpy, sample_window_width, eval_window_width, req_ta
     return np.array(x_list), np.array(y_list)
 
 
-def balance_dataset(X, y):
+def balance_dataset(X, y, neg_to_pos_ratio=1):
     positive_samples = X[y == True]
     negative_samples = X[y == False]
 
     pos_count = positive_samples.shape[0]
 
-    if negative_samples.shape[0] > pos_count:
-        negative_samples = negative_samples[:pos_count]
+    if negative_samples.shape[0] > pos_count*neg_to_pos_ratio:
+        negative_samples = negative_samples[:pos_count*neg_to_pos_ratio]
 
     X = np.concatenate((positive_samples, negative_samples))
     y = np.array([True] * positive_samples.shape[0] +
                  [False] * negative_samples.shape[0])
 
     return X, y
+
+
+def train_model(X_train, y_train):
+    model = RandomForestClassifier(n_jobs=-1)
+    model.fit(X_train, y_train)
+    return model
+
+
+def test_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+
+    real_true_cnt, real_false_cnt = y_test[y_test ==
+                                           True].shape[0], y_test[y_test == False].shape[0]
+
+    pred_true_cnt, pred_false_cnt = y_pred[y_pred ==
+                                           True].shape[0], y_pred[y_pred == False].shape[0]
+
+    print(f"Real vs predicted True: {real_true_cnt} vs {pred_true_cnt}")
+    print(f"Real vs predicted False: {real_false_cnt} vs {pred_false_cnt}")
+
+    print(metrics.classification_report(y_test, y_pred))
+
+
+if __name__ == '__main__':
+
+    train_df = pd.read_csv(
+        'data/websocket/trades.csv')
+
+    pivot = 70000
+
+    train_data = train_df[:pivot].to_numpy()
+    test_data = train_df[pivot:].to_numpy()
+
+    X_train, y_train = prepare_dataset(train_data, 200, 18000)
+    X_test, y_test = prepare_dataset(test_data, 200, 18000)
+
+    X_train, y_train = balance_dataset(X_train, y_train, 4)
+    X_test, y_test = balance_dataset(X_test, y_test, 4)
+
+    model = train_model(X_train, y_train)
+
+    test_model(model, X_test, y_test)
